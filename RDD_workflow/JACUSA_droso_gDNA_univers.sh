c19717761d4a741015f35f54732a5462929362eb
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-07-25 14:26:17 +0200

    Removing SLURM references and hard-coded path

--- RDD_workflow/JACUSA_droso_gDNA_univers.sh
+++ RDD_workflow/JACUSA_droso_gDNA_univers.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#SBATCH -n 10
-#SBATCH -N 1
-#SBATCH --mem=30G
-
-module load java
 
 export INP1=$1
 export INP2=$2
@@ -12,6 +7,4 @@ export OUT=$3
 #-RDDs-----------
 
 #gDNA_VS_siADAR-1_2.bed
-java -Xmx20g -jar /home/cdieterich/software/JACUSA_v1.2.2.jar call-2 -s -c 5 -P UNSTRANDED,FR-FIRSTSTRAND -p 10 -W 1000000 -F 1024 --filterNM_1 5 --filterNM_2 5 -a D,M,Y,H:1 -T 1.15 -r ${OUT} ${INP1} ${INP2}
-
-
+java -Xmx20g -jar JACUSA_v1.2.2.jar call-2 -s -c 5 -P UNSTRANDED,FR-FIRSTSTRAND -p 10 -W 1000000 -F 1024 --filterNM_1 5 --filterNM_2 5 -a D,M,Y,H:1 -T 1.15 -r ${OUT} ${INP1} ${INP2}
